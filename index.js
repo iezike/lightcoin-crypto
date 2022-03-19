35688f9058ed208ec3db87feb4562eb9f7d35073
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-19 08:27:02 -0400

    Intial commit for LightCoin

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
 
+}
+
+const myAccount = new Account("snow-patrol");
+
+
+class Transaction  {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account
+  }
+
+}
+
+// Withdrawal Class
+class Withdrawal extends Transaction {
+  
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;  //finalize each instance of Withdrawal
+  }
+
+}
+
+// Deposit Class
+class Deposit extends Transaction {
+
+  commit() {
+    this.account.balance += this.amount; //finalize each instance of deposit
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
+
+t3 = new Deposit(120, myAccount);
+t3.commit();
+console.log('Trasaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
